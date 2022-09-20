44e038aac7daab742816d123e3c02ad879d7ce9e
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-09-20 00:02:34 -0400

    diretivas

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <p>Sem a diretiva</p>
- <p v-texto="{cor:'red', tamanhoFonte:'200%', totalCaracteres:15}"> Minha primeira diretiva customizada</p>
- <p v-texto> Teste</p>
+  <p v-texto="{ cor: 'red', tamanhoFonte: '200%', totalCaracteres: 15 }">
+    Minha primeira diretiva customizada
+  </p>
+  <p v-texto="configuracaoTexto">Teste</p>
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
- 
-}
+  name: "App",
+  data: () => ({
+    configuracaoTexto: {
+      cor: "blue",
+      tamanhoFonte: "300%",
+      totalCaracteres: 4,
+    },
+  }),
+};
 </script>
 
 <style>
-
 </style>
