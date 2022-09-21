7843bb6d766a0747170a3e3fbfb048ee5077352e
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-09-21 08:07:37 -0400

    diretivas globais customizadas

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@
   </p>
   <p v-texto="configuracaoTexto">Teste</p>
   <div v-posicao:absolute="100" v-texto="{cor:'green', totalCaracteres: 100}"> Olá, eu sou um binding de argumento e valor</div>
+  <span>Código A:</span> <input type="text"><span v-informacao:simples.umClickMouse="'Você encontrará o codigo A na parte frontal do equipamento'">i</span>
+  <br>
+  <span>Código B:</span> <input type="text"><span v-informacao:destacado.doisClicksMouse.sairAutomaticamente="'Você encontrará o codigo B na parte traseira do equipamento, abaixo da tampa de proteção'">i</span>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -45,16 +45,97 @@ Vue.directive('texto', {
     }
 });
 
-Vue.directive('posicao',{
-    created(el, binding){
-        console.log(el, binding.arg, binding.value);
+Vue.directive('posicao', {
+    created(el, binding) {
+        // console.log(el, binding.arg, binding.value);
         const posicoesPossiveis = ['relative', 'fixed', 'absolute'];
 
 
-        if(posicoesPossiveis.includes(binding.arg)){
+        if (posicoesPossiveis.includes(binding.arg)) {
             el.style.position = binding.arg;
             el.style.top = `${binding.value}px`;
         }
+
+
+    }
+});
+
+Vue.directive('informacao', {
+    created(el, binding) {
+        console.log(el, binding.arg, binding.modifiers, binding.value);
+        let funcao = null
+        if (binding.arg == undefined || binding.arg === 'simples') {
+             funcao = function () {
+                // console.log(event);
+                let informacaoSpan = document.createElement('span');
+                informacaoSpan.style.position = 'absolute';
+                informacaoSpan.style.background = '#ddd';
+                informacaoSpan.style.padding = '2px';
+                informacaoSpan.innerText = binding.value;
+
+                el.appendChild(informacaoSpan);
+
+                informacaoSpan.addEventListener('click', (event) => {
+                    event.stopPropagation();
+                    informacaoSpan.remove();
+                });
+                if(binding.modifiers['sairAutomaticamente']){
+                    setTimeout(()=>{
+                        informacaoSpan.remove();
+                    },3000);
+                }
+            }
+        }
+
+        if( binding.arg === 'destacado'){
+            funcao = function(){
+                let informacaoDivContainer = document.createElement('div');
+                let informacaoDiv = document.createElement('div')
+                informacaoDiv.innerText = binding.value;
+    
+                informacaoDivContainer.style.with = '100%';
+                informacaoDivContainer.style.height = '100%';
+                informacaoDivContainer.style.display = 'flex';
+                informacaoDivContainer.style.flexDirection = 'row';
+                informacaoDivContainer.style.alignItems = 'center';
+                informacaoDivContainer.style.justifyContent = 'center';
+                informacaoDivContainer.style.background = '#ccc';
+                informacaoDivContainer.style.position = 'absolute';
+                informacaoDivContainer.style.top = '0';
+                informacaoDivContainer.style.zIndex = '10';
+    
+                informacaoDiv.style.padding = '30px';
+                informacaoDiv.style.background = '#fba';
+    
+                informacaoDivContainer.appendChild(informacaoDiv);
+    
+                el.appendChild(informacaoDivContainer);
+                informacaoDivContainer.addEventListener('click', (event) => {
+                    event.stopPropagation();
+                    informacaoDivContainer.remove();
+                });
+                if(binding.modifiers['sairAutomaticamente']){
+                    setTimeout(()=>{
+                        informacaoDivContainer.remove();
+                    },3000);
+                }
+               
+            }
+            
+
+        }
+
+
+        if (binding.modifiers['umClickMouse']) {
+            el.addEventListener('click', funcao);
+        }
+        if (binding.modifiers['doisClicksMouse']) {
+            el.addEventListener('dblclick', funcao);
+        }
+
+        // funcao();
+
+
 
 
     }
